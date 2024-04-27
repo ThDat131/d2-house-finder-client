cb20f6a7c7040c01e894c8555493215dfddc63b9
ThDat131 <truongthanhdat2002@gmail.com>
2024-04-27 15:21:59 +0700

    refactor: WIP Update password

--- src/views/GeneralManagement/UpdatePassword/UpdatePassword.tsx
+++ src/views/GeneralManagement/UpdatePassword/UpdatePassword.tsx
@@ -12,19 +12,37 @@ import { useTranslation } from 'react-i18next'
 import * as Yup from 'yup'
 import { useAppSelector } from '../../../app/hooks'
 import { RootState } from '../../../app/store'
+import { HttpService } from '../../../api/HttpService'
 
 const UpdatePassword = (): JSX.Element => {
   const { t } = useTranslation()
   const auth = useAppSelector((state: RootState) => state.auth.user)
+  const { authHttpService } = new HttpService()
 
   const initialValues = {
     oldPassword: '',
     newPassword: '',
+    reNewPassword: '',
   }
 
-  const onSubmit = () => {}
+  const onSubmit = () => {
+    console.log(formik.values)
+  }
 
-  const validationSchema = Yup.object().shape({})
+  const validationSchema = Yup.object().shape({
+    oldPassword: Yup.string().required(
+      t('generalManagement.updatePassword.youCantLeaveThisEmpty'),
+    ),
+    newPassword: Yup.string().required(
+      t('generalManagement.updatePassword.youCantLeaveThisEmpty'),
+    ),
+    reNewPassword: Yup.string()
+      .required(t('generalManagement.updatePassword.youCantLeaveThisEmpty'))
+      .oneOf(
+        [Yup.ref('newPassword')],
+        t('generalManagement.updatePassword.passwordMustMatch'),
+      ),
+  })
   const formik = useFormik({
     initialValues,
     validationSchema,
@@ -39,65 +57,92 @@ const UpdatePassword = (): JSX.Element => {
         </Typography>
       </Box>
       <Container>
-        <Stack spacing={5}>
-          <Grid container item alignItems={'center'}>
-            <Grid item textAlign={'center'} xs={2}>
-              <Typography>
-                {t('generalManagement.updatePassword.oldPassword')}
-              </Typography>
+        <form onSubmit={formik.handleSubmit}>
+          <Stack spacing={5}>
+            <Grid container item alignItems={'center'}>
+              <Grid item xs={2}>
+                <Typography>
+                  {t('generalManagement.updatePassword.oldPassword')}
+                </Typography>
+              </Grid>
+              <Grid item xs={10}>
+                <TextField
+                  id="oldPassword"
+                  name="oldPassword"
+                  type="password"
+                  value={formik.values.oldPassword}
+                  onChange={formik.handleChange}
+                  error={
+                    formik.touched.oldPassword &&
+                    Boolean(formik.errors.oldPassword)
+                  }
+                  helperText={
+                    formik.touched.oldPassword && formik.errors.oldPassword
+                  }
+                  size="small"
+                  fullWidth
+                />
+              </Grid>
             </Grid>
-            <Grid item xs={10}>
-              <TextField
-                id="oldPassword"
-                name="oldPassword"
-                type="password"
-                value={formik.values.oldPassword}
-                onChange={formik.handleChange}
-                error={
-                  formik.touched.oldPassword &&
-                  Boolean(formik.errors.oldPassword)
-                }
-                helperText={
-                  formik.touched.oldPassword && formik.errors.oldPassword
-                }
-                size="small"
-                fullWidth
-              />
+            <Grid container item alignItems={'center'}>
+              <Grid item xs={2}>
+                <Typography>
+                  {t('generalManagement.updatePassword.newPassword')}
+                </Typography>
+              </Grid>
+              <Grid item xs={10}>
+                <TextField
+                  id="newPassword"
+                  name="newPassword"
+                  type="password"
+                  value={formik.values.newPassword}
+                  onChange={formik.handleChange}
+                  error={
+                    formik.touched.newPassword &&
+                    Boolean(formik.errors.newPassword)
+                  }
+                  helperText={
+                    formik.touched.newPassword && formik.errors.newPassword
+                  }
+                  size="small"
+                  fullWidth
+                />
+              </Grid>
             </Grid>
-          </Grid>
-          <Grid container item alignItems={'center'}>
-            <Grid item textAlign={'center'} xs={2}>
-              <Typography>
-                {t('generalManagement.updatePassword.newPassword')}
-              </Typography>
+            <Grid container item alignItems={'center'}>
+              <Grid item xs={2}>
+                <Typography>
+                  {t('generalManagement.updatePassword.reNewPassword')}
+                </Typography>
+              </Grid>
+              <Grid item xs={10}>
+                <TextField
+                  id="reNewPassword"
+                  name="reNewPassword"
+                  type="password"
+                  value={formik.values.reNewPassword}
+                  onChange={formik.handleChange}
+                  error={
+                    formik.touched.reNewPassword &&
+                    Boolean(formik.errors.reNewPassword)
+                  }
+                  helperText={
+                    formik.touched.reNewPassword && formik.errors.reNewPassword
+                  }
+                  size="small"
+                  fullWidth
+                />
+              </Grid>
             </Grid>
-            <Grid item xs={10}>
-              <TextField
-                id="newPassword"
-                name="newPassword"
-                type="password"
-                value={formik.values.newPassword}
-                onChange={formik.handleChange}
-                error={
-                  formik.touched.newPassword &&
-                  Boolean(formik.errors.newPassword)
-                }
-                helperText={
-                  formik.touched.newPassword && formik.errors.newPassword
-                }
-                size="small"
-                fullWidth
-              />
+            <Grid container item xs={12}>
+              <Grid item xs={12}>
+                <Button type="submit" fullWidth variant="contained">
+                  {t('generalManagement.updatePassword.update')}
+                </Button>
+              </Grid>
             </Grid>
-          </Grid>
-          <Grid container item xs={12}>
-            <Grid item xs={12}>
-              <Button fullWidth variant="contained">
-                {t('generalManagement.updatePassword.update')}
-              </Button>
-            </Grid>
-          </Grid>
-        </Stack>
+          </Stack>
+        </form>
       </Container>
     </>
   )
